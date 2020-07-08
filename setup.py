2d3e83068901a0939895d8759bc9ecebd162e176
Felix Steinmetz <felix.steinmetz1@gmx.de>
2020-07-08 16:03:03 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(name='nepertools',
+                 version='1.0',
+                 description='Parsing Neper tess files',
+                 author='Felix Steinmetz',
+                 author_email='fsteinme@rhrk.uni-kl.de',
+                 packages=setuptools.find_packages(),
+                 zip_safe=False)
