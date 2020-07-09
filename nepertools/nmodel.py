147d9362b7e6380c67217f2f2c1267fc045ce2cb
Felix Steinmetz <felix.steinmetz1@gmx.de>
2020-07-09 11:27:43 +0200

    initial commit nmodel

---
+++ nepertools/nmodel.py
@@ -0,0 +1,8 @@
+#-------------------------------------------------------------------
+#Nucleation Model to create initial points for FEAP Input Skript 
+#Mesh + TessFile needes as Input 
+#-------------------------------------------------------------------
+
+#2D Version
+
+#3D Version
